0509985315fc2d1bb90fabf47567fc25dceea4b3
Salihou18 <skingue0906@gmail.com>
2025-01-07 22:23:56 -0500

    test

--- camivgal/app/page.jsx
+++ camivgal/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main>
         <div className="text-grey-500 flex w-full justify-center bg-blue-500" >
-            Soyez la bienvenue chez camivgal, le musée qui joint la culture et l'art du Cameroun, de la Cote D'Ivoire et du Sénégal
+            Bienvenue chez Camivgal, pour decouvrir le Cameroun, la Cote D'Ivoire et le Sénégal!
         </div>
         </main>
     );
