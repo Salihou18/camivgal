f8df7c6435b5599aa2b826a94a898582075f98be
Salihou18 <skingue0906@gmail.com>
2025-01-21 11:46:15 -0500

    correction en classe du 21/01

--- camivgal/app/page.jsx
+++ camivgal/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Footer from '@/components/Footer';
 import footer from '@/public/img/Footer.webp' 
-import Document from '@/components/Document';
+
 
 
 export default function Home() {
@@ -11,22 +11,12 @@ export default function Home() {
              <div className='footer bg-cover bg-center' style={{ backgroundImage: `url(${footer.src})` , height: '400px' }}>
                 <div className=" flex items-center justify-center w-full h-full  font-serif  " >
 
-                    <p className='text-xl text-white'> Bienvenue chez Camivgal, pour découvrir le Cameroun, la Cote D'Ivoire et le Sénégal!</p> 
+                    <p className='text-3xl text-white text-center leading-relaxed font-bold animate-scan'> Bienvenue chez Camivgal, pour découvrir le Cameroun, la Cote D'Ivoire et le Sénégal!</p> 
                     
-            </div>
-        </div>
-
-                    <pre className='text-3xl text-white text-center leading-relaxed font-bold animate-scan '> Bienvenue chez Camivgal pour découvrir <br /> Cameroun, Cote D'Ivoire, Sénégal!</pre> 
                 </div>
-
-
              </div>
-            
-            
-
              </div>
-
-
+    
         </main>
 
     );
