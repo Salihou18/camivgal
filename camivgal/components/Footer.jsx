2f421256750c39da039d486a22784433c164f2d7
Alaminbara <baradjig@hotmail.com>
2025-01-24 01:31:23 -0500

    avancement sur la page d acceuil.

--- camivgal/components/Footer.jsx
+++ camivgal/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="bg-blue-500 text-white text-center p-4 , mt-20">
 
-        <div className='text-3xl flex items-center justify-center span: 10' >Nos Services</div>
+        
             <nav>
                 <ul className="flex justify-center items-center flex-wrap gap-4 list-none p-0 m-0">
                     <li><a href="#" className="text-white font-bold hover:text-gray-400">À propos</a></li>
