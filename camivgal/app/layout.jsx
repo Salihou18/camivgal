8465b0084086b9f6e66552019436ce044ea2c884
Salihou18 <skingue0906@gmail.com>
2025-02-28 03:36:07 -0500

    mise a jour du fichier layout.jsx

--- camivgal/app/layout.jsx
+++ camivgal/app/layout.jsx
@@ -1,29 +1,28 @@
-"use client";
+'use client';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Inter } from 'next/font/google';
-import { useState } from "react";
+import { useState } from 'react';
 import './globals.css';
-import Activites from "@/components/Activites";
-import Cameroun from "@/components/Cameroun";
-import Contact from "@/components/Contact";
-import Cote_D_Ivoire from "@/components/Cote_D_Ivoire";
-import Senegal from "@/components/Senegal";
+import Activites from '@/components/Activites';
+import Cameroun from '@/components/Cameroun';
+import Contact from '@/components/Contact';
+import Cote_D_Ivoire from '@/components/Cote_D_Ivoire';
+import Senegal from '@/components/Senegal';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-    const [page, setPage] = useState("home");
-
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="flex flex-col min-h-screen">
-            <Header changePage={setPage} />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    );
+  const [page, setPage] = useState('home');
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          <Header changePage={setPage} />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
 }
