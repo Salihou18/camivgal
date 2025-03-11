4bccc4dc12be533e56723e71ea285d738d2d8851
Alaminbara <baradjig@hotmail.com>
2025-03-11 11:09:50 -0400

    Dark mode

--- camivgal/app/layout.jsx
+++ camivgal/app/layout.jsx
@@ -1,29 +1,21 @@
-"use client";
+'use client';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Inter } from 'next/font/google';
-import { useState } from "react";
+import { useState } from 'react';
 import './globals.css';
-import Activites from "@/components/Activites";
-import Cameroun from "@/components/Cameroun";
-import Contact from "@/components/Contact";
-import Cote_D_Ivoire from "@/components/Cote_D_Ivoire";
-import Senegal from "@/components/Senegal";
+import MyContextProvider from '@/provider/MyContextProvider';
+import Body from '@/components/Body';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-    const [page, setPage] = useState("home");
-
-    return (
-        <html lang='en'>
-            <body className={inter.className}>
-                <div className="flex flex-col min-h-screen">
-                    <Header changePage={setPage} />
-                     <main className="flex-1">{children}</main>
-                    <Footer />
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <MyContextProvider>
+        {' '}
+        <Body children={children} />
+      </MyContextProvider>
+    </html>
+  );
 }
