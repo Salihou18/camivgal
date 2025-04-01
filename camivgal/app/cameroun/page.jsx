ce705f5e53ad0d5d0b9ce41c3c0a7c5220b33c78
Salihou18 <skingue0906@gmail.com>
2025-04-01 10:32:12 -0400

    save changes

--- camivgal/app/cameroun/page.jsx
+++ camivgal/app/cameroun/page.jsx
@@ -1,5 +1,7 @@
 import Cameroun from '@/components/Cameroun';
 export const metadata = {
+  metadataBase: new URL('http://acme.com'),
+
   title: 'cameroun | Camivgal',
   description: 'Page cameroun',
   openGraph: {
